770df1167c01cb9dd7c69561d858bb9da57b0daf
Edvinas <158054355+edvinaskuc@users.noreply.github.com>
2024-02-27 11:41:07 +0200

    js

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("res");
+
+let vardas = "John";
+let pavarde = "Doe";
+let amzius = "25m";
+const asmensKodas = "37878787878";
+let gyvenamasisMiestas = "Kaunas";
+
+console.log(
+  `${vardas} ${pavarde} (${amzius}). ${asmensKodas} ${gyvenamasisMiestas}.`
+);
+
+console.log(
+  vardas +
+    " " +
+    pavarde +
+    " " +
+    "(" +
+    amzius +
+    ")" +
+    "." +
+    " " +
+    asmensKodas +
+    " " +
+    gyvenamasisMiestas +
+    "."
+);
