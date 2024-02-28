13a25e963a25e21556073dec070edfb66dbb0d60
Edvinas <158054355+edvinaskuc@users.noreply.github.com>
2024-02-28 10:48:48 +0200

    jsjj

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-// SLAPTAŽODIS:
-// 1. Jeigu slaptažodis yra trumpesnis už 16 simbolių, tai parašyti jog „Slaptažodis yra per trumpas. Jis privalo būti bent 16 simbolių ilgumo."
-// 2. Jeigu slaptažodis yra ilgesnis už 15 simbolių, tai:
-// 2.1. Patikrinti ar jis yra ilgesnis už 20 simbolių. Jeigu ilgesnis, tai parašyti: „Slaptažodis tinkamas".
-// 2.2. Jeigu jis nėra ilgesnis už 20 simbolių, tai parašyti jog: „Slaptažodis yra tinkamas. Tačiau rekomenduojama jog jis būtų bent 21 simbolio ilgumo."
+// AMŽIUS
+// Pagal amžiu surašyti į kurią klasė eina mokinys:
+// 1. Iki 6 metų į mokyklą neina.
+// 2. 7-10 metų eina į pradinę klasę.
+// 3. 11-14 metų eina į pagrindinę.
+// 4. 15-18 metų eina į gimnaziją.
+// 5. 19+ mokyklą baigė.
 
-// 3. Jeigu slaptažodis neturi grotažymių (#), tai parašyti: „Slaptažodis privalo turėti grotažymes"
+let amzius = "amzius";
 
-let password = "slaptažodis";
-
-if (password.length < 16) {
-  console.log(
-    "Slaptažodis yra per trumpas. Jis privalo būti bent 16 simbolių ilgumo."
-  );
-} else if (password.length > 15) {
-  if (password.length > 20) {
-    console.log("Slaptažodis tinkamas");
-  } else {
-    console.log(
-      "Slaptažodis yra tinkamas. Tačiau rekomenduojama jog jis būtų bent 21 simbolio ilgumo."
-    );
-  }
-}
-
-if (!password.includes("#")) {
-  console.log("Slaptažodis privalo turėti grotažymes");
+if (amzius < 6) {
+  console.log("į mokyklą neina.");
+} else if (amzius > 7 && amzius < 10) {
+  console.log("eina į pradinę klasę");
+} else if (amzius > 11 && amzius < 14) {
+  console.log("eina į pagrindinę");
+} else if (amzius > 15 && amzius < 18) {
+  console.log("eina į gimnaziją");
+} else if (amzius > 19) {
+  console.log("mokyklą baigė.");
 } else {
-  console.log("Slaptažodis tinkamas");
+  console.log("Įveskite savo amžių.");
 }
