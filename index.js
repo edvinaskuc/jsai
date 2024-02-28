394b77924a132e679bef61f326aac4d69361b0a0
Edvinas <158054355+edvinaskuc@users.noreply.github.com>
2024-02-28 11:31:42 +0200

    updt

--- index.js
+++ index.js
@@ -6,25 +6,44 @@
 // 4. 15-18 metų eina į gimnaziją.
 // 5. 19+ mokyklą baigė.
 
-let amzius = 6;
+let amzius = 122;
 
-if (amzius <= 0) {
+if (amzius < 0) {
   console.log("Įvestas amžius yra per mažas.");
 } else if (amzius <= 6) {
   console.log("į mokyklą neina.");
-} else if (amzius >= 7 && amzius <= 10) {
+} else if (amzius <= 10) {
   console.log("eina į pradinę klasę");
-} else if (amzius >= 11 && amzius <= 14) {
+} else if (amzius <= 14) {
   console.log("eina į pagrindinę");
-} else if (amzius >= 15 && amzius <= 18) {
+} else if (amzius <= 18) {
   console.log("eina į gimnaziją");
-} else if (amzius >= 19 && amzius < 120) {
+} else if (amzius <= 19) {
   console.log("mokyklą baigė.");
-} else if (amzius >= 120) {
-  console.log("Įvestas amžius yra per didelis.");
+} else if ((amzius) => 120) {
+  console.log("įvestas amžius yra per didelis..");
 } else {
   console.log("Įveskite savo amžių.");
 }
 
 // 6.1. Jeigu amžius yra mažiau nei 0, tai parašyti jog įvestas amžius yra per mažas.
 // 6.2. Jeigu amžius yra daugau nei 120, tai parašyti jog įvestas amžius yra per didelis.
+
+// 7.1. Jeigu amžius yra 6 metai, tai parašyti: "Į mokyklą tikriausiai neina, tačiau gali būti ir pirmokas."
+// 7.2. Jeigu amžius yra 10 metai, tai parašyti: "Tikriausiai mokosi pradinėje, tačiau gali būti ir penktokas."
+// 7.3. Jeigu amžius yra 14 metai, tai parašyti: "Tikriausiai mokosi pagrindinėje, tačiau gali būti ir devintokas."
+// 7.4. Jeigu amžius yra 18 metai, tai parašyti: "Tikriausiai mokosi gimnazijoje, tačiau mokyklą gali būti ir baigęs."
+
+// let amzius = 19
+
+// if(amzius < 6) {
+//     console.log('į mokyklą neina')
+// } else if(amzius <= 10) {
+//     console.log('eina į pradinę klasę')
+// } else if(amzius <= 14) {
+//     console.log('eina į pagrindine')
+// } else if(amzius <= 18) {
+//     console.log('eina į gimnazija')
+// } else {
+//     console.log('mokyklą baigė')
+// }
