c5af68f6e6337f1e78b34fe1a74a74fc4b5d592a
Edvinas <158054355+edvinaskuc@users.noreply.github.com>
2024-02-28 12:06:44 +0200

    promp

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-// AMŽIUS
-// Pagal amžiu surašyti į kurią klasė eina mokinys:
-// 1. Iki 6 metų į mokyklą neina.
-// 2. 7-10 metų eina į pradinę klasę.
-// 3. 11-14 metų eina į pagrindinę.
-// 4. 15-18 metų eina į gimnaziją.
-// 5. 19+ mokyklą baigė.
+let amzius = prompt("Įveskite savo amžių");
 
-let amzius = 18;
-
-if (amzius < 0) {
+if (isNaN(amzius)) {
+  console.log("Netinkamai nurodytas amžius, amžius privalo būti skaičius.");
+} else if (amzius < 0) {
   console.log("Įvestas amžius yra per mažas.");
-} else if (amzius <= 6) {
+} else if (amzius < 6) {
   console.log("į mokyklą neina.");
-} else if (amzius <= 10) {
+} else if (amzius < 7) {
+  console.log("Į mokyklą tikriausiai neina, tačiau gali būti ir pirmokas.");
+} else if (amzius < 10) {
   console.log("eina į pradinę klasę");
-} else if (amzius <= 14) {
+} else if (amzius < 11) {
+  console.log("Tikriausiai mokosi pradinėje, tačiau gali būti ir penktokas.");
+} else if (amzius < 14) {
   console.log("eina į pagrindinę");
+} else if (amzius < 15) {
+  console.log(
+    "Tikriausiai mokosi pagrindinėje, tačiau gali būti ir devintokas."
+  );
 } else if (amzius <= 18) {
   console.log("eina į gimnaziją");
 } else if (amzius <= 19) {
+  console.log(
+    "Tikriausiai mokosi gimnazijoje, tačiau mokyklą gali būti ir baigęs."
+  );
+} else if (amzius <= 19) {
+  console.log("mokyklą baigė.");
+} else if ((amzius) => 19) {
   console.log("mokyklą baigė.");
 } else if ((amzius) => 120) {
   console.log("įvestas amžius yra per didelis..");
 } else {
-  console.log("Įveskite savo amžių.");
+  console.log("Netinkamai nurodytas amžius, amžius privalo būti skaičius.");
 }
 
-// 6.1. Jeigu amžius yra mažiau nei 0, tai parašyti jog įvestas amžius yra per mažas.
-// 6.2. Jeigu amžius yra daugau nei 120, tai parašyti jog įvestas amžius yra per didelis.
-
-// 7.1. Jeigu amžius yra 6 metai, tai parašyti: "Į mokyklą tikriausiai neina, tačiau gali būti ir pirmokas."
-// 7.2. Jeigu amžius yra 10 metai, tai parašyti: "Tikriausiai mokosi pradinėje, tačiau gali būti ir penktokas."
-// 7.3. Jeigu amžius yra 14 metai, tai parašyti: "Tikriausiai mokosi pagrindinėje, tačiau gali būti ir devintokas."
-// 7.4. Jeigu amžius yra 18 metai, tai parašyti: "Tikriausiai mokosi gimnazijoje, tačiau mokyklą gali būti ir baigęs."
+// 8. Jeigu įvestas ne amžius (t.y. ne skaičius), tai parašyti: "Netinkamai nurodytas amžius, amžius privalo būti skaičius."
+// 9. Panaudoti prompt funkciją amžiui įvesti.
